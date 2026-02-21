f89b0dc5bc47ce8160fe644411007ebde5d511b5
haarini-31 <haarinichitravelmurugan@gmail.com>
2026-02-21 23:15:10 +0530

    Fix MLflow authentication using dotenv, install package with setup.py, and stabilize DVC evaluation stage

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8-slim-buster
+
+RUN apt get update -y && apt install awscli -y
+WORKDIR /app
+
+COPY . /app
+RUN pip install -r requirements.txt
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def predictRoute():
         return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
     app.run(host='0.0.0.0', port=5000, debug=True)
